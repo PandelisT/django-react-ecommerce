5d8f04acdd7f659a2c7e0290a4a3d3adf2801d36
PandelisT <pandeli@nerdypandy.com>
2021-03-05 10:54:27 +1100

    Added redux state management

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../Components/Rating'
-import axios from 'axios'
+import Loader from '../Components/Loader'
+import Message from '../Components/Message'
+import {listProductDetails} from '../actions/productActions'
 
 function ProductScreen({ match }) {
-
-    const [product, setProduct] = useState([])
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state => state.productDetails)
+    const {loading, error, product} = productDetails
 
     useEffect(()=> {
-        async function fetchProduct(){
-            const { data } = await axios.get(`/api/products/${match.params.id}`)
-            setProduct(data)
-        }
+        dispatch(listProductDetails(match.params.id))
 
-        fetchProduct()
-    }, [])
+    }, [dispatch, match])
 
     return (
         <div>
             <Link to="/" className='btn btn-ligth my-3'>Go Back</Link>
-            <Row>
+            {loading ?
+                <Loader />
+                : error
+                ? <Message variant='danger'>{error}</Message>
+            :(
+                <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid/>
                 </Col>
@@ -71,6 +76,13 @@ function ProductScreen({ match }) {
                     
                 </Col>
             </Row>
+
+            )
+            }
+
+
+
+ 
         </div>
     )
 }
