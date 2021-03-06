d731033e25accc104f1798c2860342ae0ffed257
PandelisT <pandeli@nerdypandy.com>
2021-03-06 12:19:57 +1100

    Added JWT to backend

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from base.models import Product
+from base.products import products
+from base.serializers import ProductSerializer
+from rest_framework import status
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer
-
-# Create your views here.
-
-@api_view(['GET'])
-def getRoutes(request):
-    return Response('Hello')
 
 @api_view(['GET'])
 def getProducts(request):
